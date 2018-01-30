8129429cfc02c97d7d8511586680477fd085d8d8
littleweh <adakao@gmail.com>
2018-01-30 15:48:21 +0800

    1. register API Connection, 2. Login API connection

--- ChatObjc/RegisterAPIConnectionManager.m
+++ ChatObjc/RegisterAPIConnectionManager.m
@@ -1,14 +1,9 @@
 //
-//  RegisterViewController.h
+//  RegisterAPIConnectionManager.m
 //  ChatObjc
 //
 //  Created by 典萱 高 on 2018/1/30.
 //  Copyright © 2018年 LostRfounds. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
-
-@interface RegisterViewController : UIViewController
-
-@end
--- ChatObjc/VC/RegisterViewController.m
+++ ChatObjc/VC/RegisterViewController.m
@@ -8,6 +8,7 @@
 
 #import "RegisterViewController.h"
 
+
 @interface RegisterViewController ()
 
 @end
@@ -17,10 +18,64 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
+
+    NSString *name = @ "testUser1";
+    NSString *age = @ "23";
+    NSString *pwd = @ "12345678";
+    NSString *confirmPwd = @ "123456";
+
+    NSDictionary *parameters = @{ @"userName": name,
+                                  @"age": age,
+                                  @"pwd": pwd,
+                                  @"confirmPwd": confirmPwd };
+    NSData *postData = [NSJSONSerialization dataWithJSONObject:parameters options:0 error:nil];
+    NSString *postLength = [NSString stringWithFormat:@ "%lu", (unsigned long)[postData length]];
+
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:
+                                    [NSURL URLWithString:
+                                     @"http://13.113.252.61/user/register"]];
+    request.HTTPMethod = @"POST";
+
+    [request setValue:@"application/json; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+
+    [request setHTTPBody:postData];
+
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *dataTask = [
+        session dataTaskWithRequest:request
+        completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            if (error) {
+                NSLog(@"%@", error);
+            } else {
+                NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *) response;
+                NSLog(@"%@", httpResponse);
+
+                if (httpResponse.statusCode == 200) {
+                    NSError *jsonError;
+                    NSString *jsonString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+                    NSLog(@"Response JSON = %@", jsonString);
+                    NSDictionary *jsonObject = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&jsonError];
+
+                    int sValue = [jsonObject[@"s"] intValue];
+                    if (sValue <0) {
+                        NSLog(@"%d", 123);
+
+                        NSLog(@"%@", jsonObject[@"msg"]);
+                        // ToDo: error handling
+                    } else {
+                        // sValue == 1 -> login API
+                        // ToDo: OOP
+
+
+
+                    }
+
+                }
+            }
+    }];
+    [dataTask resume];
 }
-
-
-
 
 
 @end
