d8a84fa5ae06076d66822803f02ed528a762035c
littleweh <adakao@gmail.com>
2018-01-31 15:21:21 +0800

    constant.h file

---
+++ ChatObjc/Constant.h
@@ -0,0 +1,28 @@
+//
+//  Constant.h
+//  ChatObjc
+//
+//  Created by 典萱 高 on 2018/1/31.
+//  Copyright © 2018年 LostRfounds. All rights reserved.
+//
+
+#ifndef Constant_h
+#define Constant_h
+
+// Analytics constants
+static NSString* const chatAPIUrl = @"http://13.113.252.61";
+static NSString* const registerEndPoint = @"/user/register";
+static NSString* const loginEndPoint = @"/user/login";
+static NSString* const userNearByEndPoint = @"/user/nearby";
+static NSString* const userLogOutEndPoint = @"/user/logout";
+static NSString* const chatDetailEndPoint = @"/chat/detail";
+static NSString* const sendMessageEndPoint = @"/chat/sendMessage";
+static NSString* const userName = @"userName";
+static NSString* const age = @"age";
+static NSString* const password = @"pwd";
+static NSString* const confirmPassword = @"confirmPwd";
+static NSString* const message = @"msg";
+static NSString* const type = @"type";
+
+
+#endif /* Constant_h */
